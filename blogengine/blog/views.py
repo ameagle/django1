5f3a9474dac7f929832cb2793962387351ebd27d
Anton Onischenko <ao@ixi.ru>
2021-09-21 22:53:21 +0300

    finish 5 lesson oleg molchanov https://www.youtube.com/watch?v=C_K12QRDE8k&list=PLlWXhlUMyooaDkd39pknA1-Olj54HtpjX&index=6

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from .models import Tag
 def posts_list(request):
     #return HttpResponse('<h1>Hello world1</h1>')
     posts = Post.objects.all()
     return render(request,'blog/index.html',context={'posts':posts})
 
 def posts_detail(request,slug):
-    slug = slug[:-1]
+    slug = slug[:-1] #del last symbol
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+
+########## Tag ##############
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+def tag_detail(request,slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+
 
 # Create your views here.
