7c72740a9a9ccf1b06833e344291e6f428810893
Anton Onischenko <ao@ixi.ru>
2021-09-22 23:53:45 +0300

    add lesson 7

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Tag
 from django.views.generic import View
 from .utils import ObjectDetailMixin
+from .forms import TagForm
+
 def posts_list(request):
     #return HttpResponse('<h1>Hello world1</h1>')
     posts = Post.objects.all()
@@ -31,7 +33,6 @@ class TagDetail(ObjectDetailMixin,View):
     #    tag = Tag.objects.get(slug__iexact=slug)
     #    return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
-
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
@@ -39,6 +40,20 @@ def tags_list(request):
 #def tag_detail(request,slug):
 #    tag = Tag.objects.get(slug__iexact=slug)
 #    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+class TagCreate(View):
+    def get(self,request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html',context={'form':form})
+
+    def post(self,request):
+        bound_form = TagForm(request.POST)
+        if bound_form.is_valid():
+            new_tag=bound_form.save()
+            return redirect(new_tag)
+        return render(request,'blog/tag_create.html',context={'form':bound_form})
+        #print(request.POST)
+
 
 
 
